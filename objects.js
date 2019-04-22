045f90d87d092fa9eea1c7456f1987680c398644
Sean Altemose <sean@seanaltemose.com>
2019-04-22 16:24:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,4 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 };
 
-function removeFromPlaylist(playlist, artistName)
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
