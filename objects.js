f66c7b299eeb3bba4017bc1e14704f3b5f1ef4e9
Sean Altemose <sean@seanaltemose.com>
2019-04-22 16:27:27 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  JanetJackson: "All Night Don't Stop",
+  MeredithBrooks: "Bitch"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+};
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+};
